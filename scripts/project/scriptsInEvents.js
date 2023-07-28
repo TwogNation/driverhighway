bfa531e094d86d794a09a8a4aff3638b3cbf9426
Matheus N <matheusnaschaves@gmail.com>
2023-07-28 17:10:12 -0300

    ws

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -9,9 +9,51 @@ function postText(value){
 
 const scriptsInEvents = {
 
-	async Global_sht_Event78_Act3(runtime, localVars)
+	async Start_sht_Event10_Act2(runtime, localVars)
 	{
-		postText(runtime.globalVars.finalScore)
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		
+		try{
+			const webSocket = new WebSocket('wss://arcade.stage.legacyarcade.com/ws', [token,gameId]);
+			runtime.globalVars.webSocket = webSocket;
+			webSocket.onopen = (event) =>{
+				runtime.callFunction('startendless');
+			};
+		}catch(e){
+			const textInstance = runtime.objects.ErrorText.getFirstInstance()
+			textInstance.text = "ERROR CONNECTING"
+			console.log("error connecting to server", e)
+		}
+		
+	},
+
+	async Global_sht_Event1(runtime, localVars)
+	{
+		
+	},
+
+	async Global_sht_Event2_Act1(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.onMessage = (event) => {
+			if (event.data.startsWith('s:')){
+				const score_ = event.data.split(':')[1];
+				console.log(score_);
+				runtime.globalVars.score = score_;
+				runtime.callFunction("updateScore")
+			}
+		}
+	},
+
+	async Global_sht_Event79_Act3(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.close();
+	},
+
+	async Global_sht_Event81_Act3(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:1")
 	}
 
 };
